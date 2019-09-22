884a5a9129f5527deef89b9677de5a07b118c17d
Luyin <myraous@gmail.com>
2019-09-22 16:31:14 +0900

    add helloSerivce that integrated with dynamodb

---
+++ src/services/helloService.ts
@@ -0,0 +1,39 @@
+import { DynamoDB } from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
+
+const docClient = new DynamoDB.DocumentClient();
+
+class HelloService {
+    private static _instance: HelloService;
+
+    public static getInstance(): HelloService {
+        if(!this._instance) {
+            this._instance = new HelloService();
+        }
+
+        return this._instance;
+    }
+
+    public async hello(): Promise<string> {  
+        const payload = {
+            TableName: 'hello',
+            Item: {
+                "id": uuid()
+            }
+        }
+
+        return new Promise((resolve, reject) => {
+            docClient.put(payload, (err, data) => {
+                if (err) {
+                    console.log(err, err.stack);
+                    reject(err);
+                    throw err;
+                } else {
+                    resolve('success');
+                }
+            })
+        });
+    }
+}
+
+export default HelloService;
